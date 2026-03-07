6c20f02d70803f2965246d00ceea215592cad7dd
Anton <strangepear@mail.ru>
2026-03-08 00:13:56 +0300

    ParserFactory

---
+++ LW1/src/main/java/org/example/parser/ParserFactory.java
@@ -0,0 +1,24 @@
+package org.example.parser;
+
+import org.example.model.Mission;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class ParserFactory {
+    private static ArrayList<IMissionParser> parsers = new ArrayList<>();
+    static {
+        parsers.add(new TxtParser());
+        parsers.add(new JSONParser());
+        parsers.add(new JSONParser());
+    }
+
+    public static IMissionParser getParser(File file){
+        for (IMissionParser imp : parsers){
+            if(imp.support(file)){
+                return imp;
+            }
+        }
+        throw new IllegalArgumentException("Неподдерживаемый формамт файла");
+    }
+}
