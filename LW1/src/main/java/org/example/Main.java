724315ed2f55f15c909ae1df859dd7bac326f980
Anton <strangepear@mail.ru>
2026-03-07 23:03:45 +0300

    JSONParser

--- LW1/src/main/java/org/example/Main.java
+++ LW1/src/main/java/org/example/Main.java
@@ -2,18 +2,19 @@ package org.example;
 
 import org.example.model.Mission;
 import org.example.parser.IMissionParser;
+import org.example.parser.JSONParser;
 import org.example.parser.TxtParser;
 
 import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        String filePath = ("C:/Users/LEGION/Desktop/данные/Данные о миссиях. Вариант 1/Mission A.txt");
+        String filePath = ("C:/Users/LEGION/Desktop/данные/Данные о миссиях. Вариант 1/Mission A.json");
         File file = new File(filePath);
 
         try {
-            IMissionParser parser = new TxtParser();
-
+            //IMissionParser parser = new TxtParser();
+            IMissionParser parser = new JSONParser();
             Mission mission = parser.parse(file);
             mission.printReport();
 
